a6ee620b9b8a5cc8ef4e1b93b641368bf1ed0be4
王方年 <wfnian@gmail.com>
2018-12-30 13:15:37 +0800

    .

---
+++ API试接入/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API试接入
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world\n");
+        }
+    }
+}
